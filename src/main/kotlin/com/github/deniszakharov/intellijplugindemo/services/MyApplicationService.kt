d9e78e116e42d98e80660b563e70310bb03f4de7
GitHub Action <action@github.com>
2022-09-02 11:49:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/deniszakharov/intellijplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/deniszakharov/intellijplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.deniszakharov.intellijplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.deniszakharov.intellijplugindemo.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/deniszakharov/intellijplugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/deniszakharov/intellijplugindemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.deniszakharov.intellijplugindemo.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.deniszakharov.intellijplugindemo.MyBundle
 
 class MyProjectService(project: Project) {
 
